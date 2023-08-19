042a9452f89bc7deba316efc901aa44bde2c85d8
SIMYEONAH <simya0528@naver.com>
2023-08-20 03:17:23 +0900

    background
    배경 해결..

--- movieapp/lib/models/movie_detail_model.dart
+++ movieapp/lib/models/movie_detail_model.dart
@@ -1,12 +1,13 @@
 class MovieDetailModel {
   num id;
   double voteAverage;
-  String title, overview;
+  String title, overview, posterPath;
   List<Map<String, dynamic>> genres;
 
   MovieDetailModel(
       {required this.id,
       required this.voteAverage,
+      required this.posterPath,
       required this.title,
       required this.genres,
       required this.overview});
@@ -15,6 +16,7 @@ class MovieDetailModel {
       : genres = jsonData['genres'].cast<Map<String, dynamic>>(),
         id = jsonData['id'],
         overview = jsonData['overview'],
+        posterPath = jsonData['poster_path'],
         title = jsonData['title'],
         voteAverage = jsonData['vote_average'].toDouble();
 }
--- movieapp/lib/screens/detail.screen.dart
+++ movieapp/lib/screens/detail.screen.dart
@@ -23,6 +23,7 @@ class _DetaileScreenState extends State<DetaileScreen> {
   void initState() {
     super.initState();
     movieDetail = ApiService.getDetailById(widget.id);
+    print("https://image.tmdb.org/t/p/w500${widget.backdropPath}");
   }
 
   String _extractGenres(List<Map<String, dynamic>> genres) {
@@ -31,52 +32,37 @@ class _DetaileScreenState extends State<DetaileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: Colors.white,
-        foregroundColor: Colors.green,
-        title: const Text(
-          "Back to list",
-          style: TextStyle(
-            fontSize: 24,
-          ),
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: NetworkImage(
+              "https://image.tmdb.org/t/p/w500${widget.backdropPath}"),
+          opacity: 0.4,
         ),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 50,
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.green,
+          title: const Text(
+            "Back to list",
+            style: TextStyle(
+              fontSize: 24,
+            ),
           ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Hero(
-                tag: widget.backdropPath,
-                child: Container(
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
-                        blurRadius: 15,
-                        offset: const Offset(10, 10),
-                        color: Colors.black.withOpacity(0.3),
-                      )
-                    ],
-                  ),
-                  child: Image.network(
-                      "https://image.tmdb.org/t/p/w500${widget.backdropPath}"),
-                ),
-              ),
-            ],
-          ),
-          const SizedBox(
-            height: 25,
-          ),
-          FutureBuilder(
+        ),
+        body: Column(
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            FutureBuilder(
               future: movieDetail,
               builder: (context, snapshot) {
                 if (snapshot.hasData) {
@@ -103,10 +89,13 @@ class _DetaileScreenState extends State<DetaileScreen> {
                       ),
                     ],
                   );
+                } else {
+                  return const Text("no data");
                 }
-                return const Text("...");
-              }),
-        ],
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
